f9d8526eb1da6e1bbea9423f0878328febec8c78
Niels Højen <niels.hojen@bestseller.com>
2020-11-09 15:44:34 +0100

    lets try this shit out

--- dobackup.sh
+++ dobackup.sh
@@ -4,13 +4,13 @@ if [ "${COMPRESSED}" = "true" ]; then
     FILE_NAME=/tmp/$BACKUP_NAME-`date "+%Y-%m-%d_%H-%M-%S"`.tar.gz
     echo "creating archive"
     tar -zcvf ${FILE_NAME} /data
-    echo "uploading archive to S3 [${FILE_NAME}]"
+    echo "uploading archive to S3 ..."
     aws --endpoint-url ${AWS_ENDPOINT_URL} s3 cp ${FILE_NAME} ${S3_BUCKET_URL}
     echo "removing local archive"
     rm ${FILE_NAME}
     echo "done"
 else
-    echo "uploading files to S3 [${FILE_NAME}]"
+    echo "uploading files to S3 ..."
     DATE=`date "+%Y-%m-%d"`
     if [ "${DELETE}" = "true" ]; then
         aws --endpoint-url ${AWS_ENDPOINT_URL} s3 sync --delete /data ${S3_BUCKET_URL}/${DATE}
